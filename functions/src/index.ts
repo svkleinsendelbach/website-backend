ee970e5f2bb46760fb7bddad9922f06ad3e9170e
Steven Kellner <steven.kellner@web.de>
2022-12-28 18:32:12 +0100

    Added SendContactMailFunction

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,7 @@ import { GetEventsFunction } from './regularFunctions/GetEventsFunction';
 import { GetNewsFunction } from './regularFunctions/GetNewsFunction';
 import { GetSingleNewsFunction } from './regularFunctions/GetSingleNewsFunction';
 import { VerifyRecaptchaFunction } from './regularFunctions/VerifyRecaptchaFunction';
+import { SendContactMailFunction } from './regularFunctions/SendContactMailFunction';
 
 admin.initializeApp();
 
@@ -15,3 +16,5 @@ export const getNews = createFunction((data, auth) => new GetNewsFunction(data, 
 export const getSingleNews = createFunction((data, auth) => new GetSingleNewsFunction(data, auth));
 
 export const verifyRecaptcha = createFunction((data, auth) => new VerifyRecaptchaFunction(data, auth));
+
+export const sendContactMail = createFunction((data, auth) => new SendContactMailFunction(data, auth));
---
+++ functions/src/regularFunctions/SendContactMailFunction.ts
@@ -0,0 +1,103 @@
+import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+
+import { AuthData } from 'firebase-functions/lib/common/providers/tasks';
+import { checkPrerequirements } from '../utils/checkPrerequirements';
+import { DatabaseType } from '../utils/DatabaseType';
+import { FirebaseFunction } from '../utils/FirebaseFunction';
+import { Logger } from '../utils/Logger';
+import { ParameterContainer } from '../utils/ParameterContainer';
+import { ParameterParser } from '../utils/ParameterParser';
+import { sendContactMailAccount } from '../privateKeys';
+
+export class SendContactMailFunction implements FirebaseFunction<
+    SendContactMailFunction.Parameters,
+    SendContactMailFunction.ReturnType
+> {
+
+    public parameters: SendContactMailFunction.Parameters;
+
+    private logger: Logger;
+
+    private transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: sendContactMailAccount,
+        logger: true
+    });
+
+    public constructor(data: any, auth: AuthData | undefined) {
+        this.logger = Logger.start(!!data.verbose, 'SendContactMailFunction.constructor', { data, auth }, 'notice');
+        const parameterContainer = new ParameterContainer(data, this.logger.nextIndent);
+        const parameterParser = new ParameterParser<SendContactMailFunction.Parameters>(
+            {
+                privateKey: 'string',
+                databaseType: ['string', DatabaseType.fromString],
+                senderName: 'string',
+                senderAddress: 'string',
+                receiverName: 'string',
+                receiverAddress: 'string',
+                message: 'string'
+            },
+            this.logger.nextIndent
+        );
+        parameterParser.parseParameters(parameterContainer);
+        this.parameters = parameterParser.parameters;
+    }
+
+    public async executeFunction(): Promise<SendContactMailFunction.ReturnType> {
+        this.logger.append('SendContactMailFunction.executeFunction', {}, 'info');
+        await checkPrerequirements(this.parameters, this.logger.nextIndent, 'notRequired'); 
+        const mailOptions: Mail.Options = {
+            from: `${this.parameters.senderName} <${this.parameters.senderAddress}>`,
+            to: 'steven.kellner@gmx.de', // this.parameters.receiverAddress,
+            subject: `${this.parameters.senderName}: E-Mail von der SVK-Website`,
+            text: `
+                Hallo Vertreter der/des ${this.parameters.receiverName}/s,
+                
+                diese Mail wurde von der SV Kleinsendelbach Website von ${this.parameters.senderName}, ${this.parameters.senderAddress} versendet:
+                
+                ${this.parameters.message}
+                `,
+            html: `
+                <p style="font-size: 16px;">Hallo Vertreter der/des ${this.parameters.receiverName}/s,</p>
+                <br/>
+                <p>diese Mail wurde von der SV Kleinsendelbach Website von ${this.parameters.senderName}, ${this.parameters.senderAddress} versendet:</p>
+                <br/>
+                <p>${this.parameters.message}</p>
+                `
+        };
+        return await new Promise<{ success: boolean; message: string }>(resolve => {
+            this.transporter.sendMail(mailOptions, (error, info) => {
+                if (error !== null) {
+                    return resolve({ 
+                        success: false, 
+                        message: error.toString() 
+                    });
+                }
+                return resolve({ 
+                    success: true, 
+                    message: info.response 
+                });
+            });
+        });
+    }
+}
+
+export namespace SendContactMailFunction {
+    export interface Parameters {
+        privateKey: string
+        databaseType: DatabaseType
+        senderName: string
+        senderAddress: string
+        receiverName: string
+        receiverAddress: string
+        message: string
+    }
+
+    export interface ReturnType {
+        success: boolean
+        message: string
+    }
+}
